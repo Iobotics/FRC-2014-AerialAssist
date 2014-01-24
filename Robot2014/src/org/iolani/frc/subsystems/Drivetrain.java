60481212bb1e4e9e48377248c4ccb8d431153aac
Rachael Ridao <raridao@iolani.org>
2014-01-23 16:35:01 -1000

    method init() in Drivetrain subsystem

--- Robot2014/src/org/iolani/frc/subsystems/Drivetrain.java
+++ Robot2014/src/org/iolani/frc/subsystems/Drivetrain.java
@@ -3,7 +3,8 @@
  * and open the template in the editor.
  */
 package org.iolani.frc.subsystems;
-
+import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.Victor;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
@@ -11,9 +12,21 @@ import edu.wpi.first.wpilibj.command.Subsystem;
  * @author iobotics
  */
 public class Drivetrain extends Subsystem {
+    private Victor _rVictor;
+    private Victor _lVictor;
+    
+    private RobotDrive _drive;
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
-
+    
+    public void init() {
+        _rVictor = new Victor(1);
+        _lVictor = new Victor(2);
+        
+        _drive = new RobotDrive(_rVictor.getChannel(), _lVictor.getChannel());
+        _drive.setSafetyEnabled(false);
+    }        
+    
     public void initDefaultCommand() {
         // Set the default command for a subsystem here.
         //setDefaultCommand(new MySpecialCommand());
