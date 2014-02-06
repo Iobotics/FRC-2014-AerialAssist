48cbdeee7799046e9ff016a2cada068672c6c4a2
Jeff Malins <jmalins@iolani.org>
2014-02-05 15:18:08 -1000

    Added (untested) asynchronous sensing to BallGrabber.

--- Robot2014/src/org/iolani/frc/commands/GrabBallWhenSensed.java
+++ Robot2014/src/org/iolani/frc/commands/GrabBallWhenSensed.java
@@ -5,25 +5,41 @@
  */
 package org.iolani.frc.commands;
 
+import org.iolani.frc.subsystems.BallGrabber;
+
 /**
  * Open the ball grabber and wait until the sensor detects a ball. When a ball is
  * sensed, close the grabber. This command continues to run until canceled.
  * 
  * @author jmalins
  */
-public class GrabBallWhenSensed extends CommandBase {
+public class GrabBallWhenSensed extends CommandBase implements BallGrabber.BallSensedListener {
     // states //
     private static final int sOPEN    = 1;
     private static final int sWAITING = 2;
     private static final int sCLOSE   = 3;
     private static final int sGRABBED = 4;
+    
+    private final boolean _async;
     private int _state;
     
-    public GrabBallWhenSensed() {
+    /**
+     * Constructor
+     * @param async - use asynchronous notification 
+     */
+    public GrabBallWhenSensed(boolean async) {
+        _async = async;
         // Use requires() here to declare subsystem dependencies
         requires(ballGrabber);
     }
 
+    /**
+     * Constructor.
+     */
+    public GrabBallWhenSensed() {
+        this(false);
+    }
+    
     // Called just before this Command runs the first time
     protected void initialize() {
         // Grabbed | Ball | Actions
@@ -36,10 +52,16 @@ public class GrabBallWhenSensed extends CommandBase {
         } else {
             _state = ballGrabber.isBallSensed() ? sCLOSE : sWAITING;
         }
+        
+        // add ourselves as a listener if in async mode //
+        if(_async) {
+            ballGrabber.addBallSensedListener(this);
+        }
     }
 
     // Called repeatedly when this Command is scheduled to run
-    protected void execute() {
+    // Must be synchronized since mutates state and is called from multiple threads
+    protected synchronized void execute() {
         //System.out.println("GrabCommand: " + _state + ", " + ballGrabber.isGrabbed() + ", " + ballGrabber.isBallSensed());
         switch(_state) {
             case sOPEN:
@@ -47,6 +69,10 @@ public class GrabBallWhenSensed extends CommandBase {
                 _state = sWAITING;
                 break;
             case sWAITING:
+                // if we are in async mode, the ball sensing happens via a //
+                // callback on a separate thread, so we don't need to do   //
+                // anything here in waiting state.                         //
+                if(_async) break;
                 // if we see a ball, close the grabber //
                 if(ballGrabber.isBallSensed()) {
                     // special handling: get a head start on closing //
@@ -74,6 +100,10 @@ public class GrabBallWhenSensed extends CommandBase {
 
     // Called once after isFinished returns true
     protected void end() {
+        // remove ourselves as a listener if in async mode //
+        if(_async) {
+            ballGrabber.removeBallSensedListener(this);
+        }
         // do not change the grabber state, since we don't know if the command    //
         // that interrupted us (the only way to make this command end) is opening //
         // or closing the grabber and we don't want to switch it rapidly          //
@@ -83,5 +113,17 @@ public class GrabBallWhenSensed extends CommandBase {
     // subsystems is scheduled to run
     protected void interrupted() {
         this.end();
+    }
+    
+    /**
+     * Callback used in asynchronous mode. Updates the state immediately
+     * and runs execute() if a ball is detected.
+     */
+    public void ballSensed() {
+        if(_state == sWAITING) {
+            System.out.println("Ball sensed interrupt!");
+            _state = sCLOSE;
+            this.execute();
+        }
     }
 }
