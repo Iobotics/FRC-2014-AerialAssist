aa669300ee8622f3f6e6097615d6a4030d5745ee
Jeff Malins <jmalins@iolani.org>
2014-02-05 15:35:00 -1000

    Added init() method to Catapult.

--- Robot2014/src/org/iolani/frc/subsystems/Catapult.java
+++ Robot2014/src/org/iolani/frc/subsystems/Catapult.java
@@ -14,6 +14,13 @@ public class Catapult extends Subsystem {
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
 
+    /**
+     * Initialize the catapult.
+     */
+    public void init() {
+        
+    }
+    
     public void initDefaultCommand() {
         // Set the default command for a subsystem here.
         //setDefaultCommand(new MySpecialCommand());
