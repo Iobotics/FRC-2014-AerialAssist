872b7c7453c2f6b3bed068b3576fe8695a6d8049
Jeff Malins <jmalins@iolani.org>
2014-03-12 12:00:36 -1000

    Changed RetractCatapult command to use PID position control

--- Robot2014/src/org/iolani/frc/commands/RetractCatapult.java
+++ Robot2014/src/org/iolani/frc/commands/RetractCatapult.java
@@ -6,16 +6,12 @@
 package org.iolani.frc.commands;
 
 /**
- *
+ * Retract the catapult using PID control.
  * @author jmalins
  */
 public class RetractCatapult extends CommandBase {
     
-    private static final int sRETRACT = 1;
-    private static final int sSTOP    = 2;
-    private static final int sHOLD    = 3;
-    
-    private int _state;
+    private static final double RETRACT_POSITION_DEGREES = 0.0;
     
     public RetractCatapult() {
         // Use requires() here to declare subsystem dependencies
@@ -24,41 +20,21 @@ public class RetractCatapult extends CommandBase {
 
     // Called just before this Command runs the first time
     protected void initialize() {
-        // clear any existing conditions //
-        //catapult.setHeld(false);
-        
-        _state = catapult.isRetracted()? sHOLD: sRETRACT;
+        catapult.setPositionSetpoint(RETRACT_POSITION_DEGREES);
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-        switch(_state) {
-            case sRETRACT:
-                if(catapult.isRetracted()) {
-                    _state = sSTOP;
-                    break;
-                }
-                //catapult.setLatched(true);
-                catapult.setWinchPower(1.0);
-                break;
-            case sSTOP:
-                catapult.setWinchPower(0.0);
-                _state = sHOLD;
-                // no break! //
-            case sHOLD:
-                //catapult.setHeld(true);
-                break;
-        }
     }
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return false;
+        return catapult.isRetracted() || catapult.isPositionOnTarget();
     }
 
     // Called once after isFinished returns true
     protected void end() {
-        //catapult.setHeld(false);
+        catapult.setWinchPower(0.0); // cancel PID control, stop winch //
     }
 
     // Called when another command which requires one or more of the same
