25df249338e9e18dff029879d79c82e7fe74fa78
Jeff Malins <jmalins@iolani.org>
2014-03-13 08:54:53 -1000

    Allow variable power to update repeatedly.

--- Robot2014/src/org/iolani/frc/commands/SetVariableIntakePower.java
+++ Robot2014/src/org/iolani/frc/commands/SetVariableIntakePower.java
@@ -37,7 +37,7 @@ public class SetVariableIntakePower extends CommandBase {
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return false;
+        return true;
     }
 
     // Called once after isFinished returns true
