e9403c00722d5cc40ab78fdeca3ee0b35609911e
Jeff Malins <jmalins@iolani.org>
2014-03-21 19:56:15 -1000

    Added state tracking, improved lob to launch

--- Robot2014/src/org/iolani/frc/commands/LaunchBall.java
+++ Robot2014/src/org/iolani/frc/commands/LaunchBall.java
@@ -5,6 +5,8 @@
  */
 package org.iolani.frc.commands;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
 /**
  *
  * @author jmalins
@@ -14,15 +16,16 @@ public class LaunchBall extends CommandBase {
     // states //
     private static final int sOPEN     = 1;
     private static final int sOPENWAIT = 2;
-    private static final int sFIRE     = 3;
-    private static final int sFIREWAIT = 4;
-    private static final int sDONE     = 5;
+    private static final int sLOBWAIT  = 3;
+    private static final int sFIRE     = 4;
+    private static final int sFIREWAIT = 5;
+    private static final int sDONE     = 6;
     
     private static final double OPEN_DELAY = 0.25;
+    private static final double LOB_DELAY  = 0.25;
     private static final double FIRE_DELAY = 1.0;
-    private static final double LOB_SAFE_POSITION = 22.0;
     
-    private int _state;
+    private int    _state;
     private double _timer;
         
     public LaunchBall() {
@@ -50,9 +53,18 @@ public class LaunchBall extends CommandBase {
                 _timer = this.timeSinceInitialized();
                 break;
             case sOPENWAIT:
-                if((this.timeSinceInitialized() - _timer) > OPEN_DELAY) {
+                if((this.timeSinceInitialized() - _timer) < OPEN_DELAY) break;
+                if(oi.getLobButton()) {
+                    catapult.setLob(true);
+                    _state = sLOBWAIT;
+                    _timer = this.timeSinceInitialized();
+                } else {
                     _state = sFIRE;
                 }
+                break;
+            case sLOBWAIT:
+                if((this.timeSinceInitialized() - _timer) < LOB_DELAY) break;
+                _state = sFIRE;
                 break;
             case sFIRE:
                 catapult.setWinchLatched(false);
@@ -60,19 +72,17 @@ public class LaunchBall extends CommandBase {
                 _timer = this.timeSinceInitialized();
                 break;
             case sFIREWAIT:
-                if(oi.getLobButton() && catapult.getPositionDegrees() > LOB_SAFE_POSITION) {
-                    catapult.setLob(true);
-                }
-                if((this.timeSinceInitialized() - _timer) > FIRE_DELAY) {
-                    catapult.setWinchLatched(true);
-                    catapult.setLob(false);
-                    _state = sDONE;
-                }
+                if((this.timeSinceInitialized() - _timer) < FIRE_DELAY) break;
+                // reset everything //
+                catapult.setWinchLatched(true);
+                catapult.setLob(false);
+                _state = sDONE;
                 break;
             case sDONE:
                 // won't get here //
                 break;
         }
+        SmartDashboard.putString("catapult-launch-state", this.getStateName());
     }
 
     // Make this return true when this Command no longer needs to run execute()
@@ -83,11 +93,24 @@ public class LaunchBall extends CommandBase {
 
     // Called once after isFinished returns true
     protected void end() {
+        SmartDashboard.putString("catapult-launch-state", "");
     }
 
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
         throw new IllegalStateException("Cannot interrupt ball launch");
+    }
+    
+    private String getStateName() {
+        switch(_state) {
+            case sOPEN:     return "OPEN";
+            case sOPENWAIT: return "OPENWAIT";
+            case sLOBWAIT:  return "LOBWAIT";
+            case sFIRE:     return "FIRE";
+            case sFIREWAIT: return "FIREWAIT";
+            case sDONE:     return "DONE";
+        }
+        return "UNKNOWN";
     }
 }
